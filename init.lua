e8315db0957648fdd3a03e70ed98df10022b87e4
Silvan Schmidt <sil.schmidt@proton.me>
2023-06-17 16:21:40 +0200

    init

---
+++ init.lua
@@ -0,0 +1,20 @@
+return {
+    mappings = {
+        n = {
+            ["<Left>"] = "<Nop>",
+            ["<Right>"] = "<Nop>",
+            ["<Up>"] = "<Nop>",
+            ["<Down>"] = "<Nop>",
+        },
+        i = {
+            ["<Left>"] = "<Nop>",
+            ["<Right>"] = "<Nop>",
+            ["<Up>"] = "<Nop>",
+            ["<Down>"] = "<Nop>",
+            ["<C-Left>"] = "<Nop>",
+            ["<C-Right>"] = "<Nop>",
+            ["<C-Up>"] = "<Nop>",
+            ["<C-Down>"] = "<Nop>",
+        }
+    }
+}
